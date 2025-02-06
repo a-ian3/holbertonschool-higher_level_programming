4ea2f6f14d0fa60f4958350f16ccd91d48edfb8b
a-ian3 <arianeediatta@icloud.com>
2025-02-06 12:31:23 +0000

    Ariane

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Function that exact same object"""
+
+
+def is_same_class(obj, a_class):
+    """ function that returns True if the object is
+    exactly an instance of the specified class ; otherwise False
+
+    Args:
+        obj: Object
+        a_class: class type
+    """
+    return type(obj) is a_class
