b394429213943a7e46963cd9294d9cdb841ae35f
a-ian3 <arianeediatta@icloud.com>
2025-05-12 12:10:45 +0000

    sql

--- SQL_more_queries/15-comedy_only.sql
+++ SQL_more_queries/15-comedy_only.sql
@@ -1,8 +1,8 @@
--- shows and genre!
--- select show.title, genre.name from joined tables
-SELECT tv_shows.title, tv_genres.name
-FROM tv_shows
-LEFT JOIN tv_show_genres ON tv_show_genres.show_id = tv_shows.id
-LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
-ORDER BY tv_shows.title, tv_genres.name
-;
+-- Only comedy
+-- select show names that match genre comedy
+SELECT s.title FROM tv_genres g, tv_show_genres t, tv_shows s
+WHERE g.id = t.genre_id
+    AND t.show_id = s.id
+    AND g.name = "Comedy"
+ORDER BY s.title ASC;
+; 
